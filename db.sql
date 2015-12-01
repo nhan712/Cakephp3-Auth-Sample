8e2becd521cd3e63db0b51d7f77877567bf777d4
nhan712 <chinhan712@gmail.com>
2015-12-01 12:00:45 +0700

    CakePHP 3 Authentication Sample commit

---
+++ db.sql
@@ -0,0 +1,27 @@
+﻿/* First, create our articles table: */
+CREATE TABLE articles (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50),
+    body TEXT,
+	user_id INT(11) NULL DEFAULT NULL,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
+);
+
+/* Then insert some articles for testing: */
+INSERT INTO articles (title,body,created)
+    VALUES ('The title', 'This is the article body.', NOW());
+INSERT INTO articles (title,body,created)
+    VALUES ('A title once again', 'And the article body follows.', NOW());
+INSERT INTO articles (title,body,created)
+    VALUES ('Title strikes back', 'This is really exciting! Not.', NOW());
+
+/* create our users table: */
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255),
+    role VARCHAR(20),
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
+);
---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,19 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+    
+    public function isOwnedBy($articleId, $userId)
+    {
+    	return $this->exists(['id' => $articleId, 'user_id' => $userId]);
+    }
+}
